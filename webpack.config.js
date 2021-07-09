7935310a393a5f11f59be314511e43a7c6121761
Guilherme <guilhermet213@gmail.com>
2021-07-09 10:52:40 -0300

    chore: configure webpack Sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,9 +30,9 @@ module.exports = {
         use: 'babel-loader',
       },
       {
-        test: /\.css$/,
+        test: /\.s?css$/,
         exclude: /node_modules/,
-        use: ['style-loader','css-loader'],
+        use: ['style-loader','css-loader', 'sass-loader'],
       }
     ]
   }
