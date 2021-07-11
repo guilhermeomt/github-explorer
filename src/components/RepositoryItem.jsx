0d9aae031a1f159a739a6a65d380a41aea9bb644
Guilherme <guilhermet213@gmail.com>
2021-07-11 08:16:08 -0300

    feat: retrieve repositories data from github api

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ export function RepositoryItem(props) {
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
 
-      <a href={props.repository.link}>Acessar repositório</a>
+      <a href={props.repository.html_url}>Acessar repositório</a>
     </li>
   );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
@@ -9,15 +10,22 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then((res) => res.json())
+      .then(data => setRepositories(data));
+   }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-       <RepositoryItem repository={ repository } />
-       <RepositoryItem repository={ repository } />
-       <RepositoryItem repository={ repository } />
-       <RepositoryItem repository={ repository } />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />;
+        })}
       </ul>
     </section>
 
